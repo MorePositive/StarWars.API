a309970972bfd4ec52299768f6325a3e67e27cab
MorePositive <morepositivecode@gmail.com>
2020-04-02 01:54:58 +0300

    people selecting

--- src/components/random-planet/random-planet.js
+++ src/components/random-planet/random-planet.js
@@ -34,7 +34,7 @@ export default class RandomPlanet extends Component {
   };
 
   updatePlanet() {
-    const id = Math.floor(Math.random()*2599);
+    const id = Math.floor(Math.random()*25);
     this.swapiService.getPlanet(id)
       .then(this.onPlanetLoaded)
       .catch(this.onError);
@@ -67,7 +67,8 @@ const PlanetView = ({ planet }) => {
   return (
     <React.Fragment>
       <img className="planet-image"
-        src={ `https://starwars-visualguide.com/assets/img/planets/${id}.jpg` } />
+        src={ `https://starwars-visualguide.com/assets/img/planets/${id}.jpg` } 
+        alt="planet" />
         <div>
           <h4>{ name }</h4>
           <ul className="list-group list-group-flush">
